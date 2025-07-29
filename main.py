0cc52bd1e422c11a89d757c66d457a03a043548a
miss-lera-19 <lera0podol4uk@ukr.net>
2025-07-29 23:52:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# main.py
+from keep_alive import keep_alive
+import time
+
+# Тут буде твій реальний код Telegram-бота зі стратегією
+print("Бот запущено ✅")
+
+keep_alive()
+
+while True:
+    time.sleep(30)
